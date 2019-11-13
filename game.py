ab1e31b7270ef7746b0d9287a2959bbfed7a20b1
artoorio <37396426+artoorio@users.noreply.github.com>
2019-11-13 20:34:08 +0500

    Update game.py

--- game.py
+++ game.py
@@ -5,11 +5,11 @@ class Player:
         self.history = []
         self.win = None
         
-    def make_step(self,num_in : 'Last step number', num_win : 'Number for win'):
+    def make_step(self,stones_on_table : 'Last step number', stones_for_win : 'Number for win'):
         
         '''описание логики выбора хода''' 
         
-        return num_in+1
+        return stones_on_table+1
     
     def collecting_points(self):
         'получение очков за игру'
@@ -19,21 +19,21 @@ class Player:
 class Game:
     def __init__(self, start: int, finish : int, players : list): 
         self.start = start # Starting number
-        self.finish = finish # Winning number
+        self.STONES_FOR_WIN = finish 
         self.players = players # List[player]
         self.step_result = None # Step result
-        self.last_step = start # Last step
+        self.stones_on_table = start # Last step
     
     def play(self):
         
         '''сначала играем, а потом раздаем очки'''
         
-        while self.step_result < self.finish:
+        while self.stones_on_table < self.STONES_FOR_WIN:
             for player in self.players:
-                self.step_result = player.make_step(self.last_step,self.finish) 
-                if self.step_result >= self.finish: #если кто то победил, то заканчиваем цикл
+                self.stones_on_table = player.make_step(self.stones_on_table,self.STONES_FOR_WIN) 
+                if self.stones_on_table >= self.STONES_FOR_WIN: #если кто то победил, то заканчиваем цикл
                     break
-                self.last_step = self.step_result 
+                
                 
         for player in self.players:
-            player.collecting_points() #раздача очков
+            player.collecting_points() # раздача очков
