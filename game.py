f8bc6c6b3ffd49376dea5c482f1bc8381879f7b9
artoorio <37396426+artoorio@users.noreply.github.com>
2019-11-22 20:22:31 +0500

    Update game.py
    исправил ошибки, код на данный момент рабочий

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 class Player:
     def __init__(self):
         self.nums = {}
@@ -19,13 +21,13 @@ class Player:
         2. выбор между двумя вариантами хода"""
         
         if stones_on_table+1 not in self.nums: 
-            nums[stones_on_table+1] = 0
+            self.nums[stones_on_table+1] = 0
         if stones_on_table*2 not in self.nums:
-            nums[stones_on_table*2] = 0
+            self.nums[stones_on_table*2] = 0
         if self.nums[stones_on_table+1] > self.nums[stones_on_table*2]: 
-            result = add_one_stone(stones_on_table)
+            result = self.add_one_stone(stones_on_table)
         else:
-            result = add_two_stones(stones_on_table)
+            result = self.add_two_stones(stones_on_table)
         self.history.append(result) 
         
         return result
@@ -42,16 +44,20 @@ class Player:
     
     
 class Game:
-    def __init__(self, start: int, players : list): 
-        self.STONES_FOR_WIN = 65 
+    STONES_FOR_WIN = 65
+    
+    def __init__(self, players : list): 
         self.players = players # List[player]
-        self.stones_on_table = start # Last step
+        self.stones_on_table = random.randint(1, 23) # Last step
     
     def play(self):
         
         """сначала играем, а потом раздаем очки"""
         
         win = False
+        for player in self.players:
+            player.win = False
+            player.history = []
         while not win:
             for player in self.players:
                 self.stones_on_table = player.make_step(self.stones_on_table) 
@@ -60,6 +66,7 @@ class Game:
                     win = True
                     break
                 
-                
+    def give_rewards(self):
         for player in self.players:
-            player.collecting_points() 
+            player.collecting_points()        
+        
